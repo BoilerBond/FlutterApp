5b1f86d70120bfec2a88157cf385aedd83a8113e
acezxn <acezxn@gmail.com>
2025-02-28 16:46:05 -0500

    Added more settings to notifications

--- lib/presentation/screens/dashboard/settings/app_settings/app_settings.dart
+++ lib/presentation/screens/dashboard/settings/app_settings/app_settings.dart
@@ -7,11 +7,35 @@ class AppSettings extends StatefulWidget {
 }
 
 class _AppSettingsState extends State<AppSettings> {
-  bool _isNotificationsEnabled = false;
+  bool _notificationsEnabled = false;
+  bool _matchResultNotificationEnabled = false;
+  bool _messageNotificationEnabled = false;
+  bool _eventsNotificationsEnabled = false;
 
   void _toggleNotifications(bool value) {
     setState(() {
-      _isNotificationsEnabled = value;
+      _notificationsEnabled = value;
+      _matchResultNotificationEnabled = value;
+      _messageNotificationEnabled = value;
+      _eventsNotificationsEnabled = value;
+    });
+  }
+
+  void _toggleMatchResultNotifications(bool value) {
+    setState(() {
+      _matchResultNotificationEnabled = value;
+    });
+  }
+  
+  void _toggleMessageNotifications(bool value) {
+    setState(() {
+      _messageNotificationEnabled = value;
+    });
+  }
+
+  void _toggleEventsNotifications(bool value) {
+    setState(() {
+      _eventsNotificationsEnabled = value;
     });
   }
 
@@ -25,7 +49,14 @@ class _AppSettingsState extends State<AppSettings> {
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
-            SwitchListTile(title: const Text('Enable notifications'), value: _isNotificationsEnabled, activeTrackColor: Theme.of(context).colorScheme.primary, onChanged: _toggleNotifications)
+            SwitchListTile(title: const Text('Notifications'), value: _notificationsEnabled, activeTrackColor: Theme.of(context).colorScheme.primary, onChanged: _toggleNotifications),
+            _notificationsEnabled ? Column(
+              children: [
+                SwitchListTile(title: const Text('Match result notifications'), value: _matchResultNotificationEnabled, activeTrackColor: Theme.of(context).colorScheme.primary, onChanged: _toggleMatchResultNotifications),
+                SwitchListTile(title: const Text('Messaging notifications'), value: _messageNotificationEnabled, activeTrackColor: Theme.of(context).colorScheme.primary, onChanged: _toggleMessageNotifications),
+                SwitchListTile(title: const Text('Event notifications'), value: _eventsNotificationsEnabled, activeTrackColor: Theme.of(context).colorScheme.primary, onChanged: _toggleEventsNotifications)
+              ],
+            ) : Container()
           ],
         ),
       ),
