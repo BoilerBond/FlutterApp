c32b5d741c145cc8606b35a4d375be5ad77f2ac7
NinaL129 <64241240+NinaL129@users.noreply.github.com>
2025-03-07 16:13:52 -0500

    updated delete user + info

--- lib/presentation/screens/dashboard/settings/danger_zone/danger_zone.dart
+++ lib/presentation/screens/dashboard/settings/danger_zone/danger_zone.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:datingapp/presentation/widgets/confirm_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class DangerZone extends StatefulWidget {
   const DangerZone({super.key});
@@ -44,19 +46,26 @@ class _DangerZoneState extends State<DangerZone> {
 
   void _onDeleteAccountPress(BuildContext context) {
     confirmDialog(context, () async {
-      // TODO: delete user information in Firebase
-
-      // get current user
       final user = FirebaseAuth.instance.currentUser;
       try {
-        // requires user data to be stored in paths with their uid
+        final callable = FirebaseFunctions.instance.httpsCallable('user-account-deleteAccountProfile');
+        await callable.call();
         await user?.delete();
-      }
-      on FirebaseAuthException {
+      } on FirebaseAuthException {
         // user signed-in too long ago
-        // log them out so they sign-in again
-        await FirebaseAuth.instance.signOut();
+        // need to reauthenticate
+        final googleUser = await GoogleSignIn().signIn();
+        if (googleUser == null) return;
+
+        final googleAuth = await googleUser.authentication;
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        await user!.reauthenticateWithCredential(credential);
+        await user.delete();
       }
+
       await FirebaseAuth.instance.signOut();
     });
   }
